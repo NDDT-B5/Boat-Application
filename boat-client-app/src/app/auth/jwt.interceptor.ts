b422a0b896697d5a6e224cad4b398178e69b9256
Nicolas Torchalla <nicolas@torchalla.eu>
2025-04-17 18:49:03 +0200

    feature(frontend): added handling for token expired

--- boat-client-app/src/app/auth/jwt.interceptor.ts
+++ boat-client-app/src/app/auth/jwt.interceptor.ts
@@ -1,23 +1,30 @@
 import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
 import { AuthService } from './auth.service';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log("asd");
   const authService = inject(AuthService);
-
-  
   const token = authService.getToken();
+  const router = inject(Router);
   
-  console.log(token);
-
   if (token) {
     const clonedRequest = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
       },
     });
-    return next(clonedRequest);
+    return next(clonedRequest).pipe(
+      catchError((error) => {
+        if (error.status === 401) {
+          // Token is expired or invalid
+          authService.removeToken();
+          router.navigate(['/login']);  // Redirect to login page
+        }
+        return throwError(() => error);
+      })
+    );;
   }
 
   return next(req);
